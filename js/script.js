9e59eb4cb7b20780473c9d7365ca6f55f2b8d25f
ManuelBenini <manuelbenini1905@gmail.com>
2022-04-07 10:24:18 +0200

    Aggiunto controllo sui dati inseriti dall'utente

--- js/script.js
+++ js/script.js
@@ -17,23 +17,48 @@
 
 const km = parseInt(prompt('Inserire chilometri del viaggio'));
 const age = parseInt(prompt('Inserire età del passeggero'));
+let insertOk = true;
 const priceNotDiscounted = 0.21 * km;
 let totalPrice;
+let travelOutput;
 
-
-if (age < 18){
-  totalPrice = priceNotDiscounted - (priceNotDiscounted * 20 / 100);
-} else if (age > 65){
-  totalPrice = priceNotDiscounted - (priceNotDiscounted * 40 / 100);
-} else{
-  totalPrice = priceNotDiscounted;
+if (isNaN(km)) {
+  alert('Inserire un numero per la distanza');
+  insertOk = false;
 }
 
-const totalPriceDecimal = totalPrice.toFixed(2);
 
-const travelOutput = `
-Come inserito dall'utente, il viaggio &egrave; lungo ${km} chilometri ed il passeggero ha ${age} anni. <br>
-Il prezzo totale che vi spetta con gli sconti applicati corrisponde a ${totalPriceDecimal} &euro;.
-`;
+if (isNaN(age)) {
+  alert('Inserire un numero per l\'età');
+  insertOk = false;
+}
+
+if (insertOk === true) {
+  
+  if (age < 18){
+    totalPrice = priceNotDiscounted - (priceNotDiscounted * 20 / 100);
+  } else if (age > 65){
+    totalPrice = priceNotDiscounted - (priceNotDiscounted * 40 / 100);
+  } else{
+    totalPrice = priceNotDiscounted;
+  }
+
+  const totalPriceDecimal = totalPrice.toFixed(2);
+
+  travelOutput = `
+    Come inserito dall'utente, il viaggio &egrave; lungo ${km} chilometri ed il passeggero ha ${age} anni. <br>
+    Il prezzo totale che vi spetta con gli sconti applicati corrisponde a ${totalPriceDecimal} &euro;.
+  `;
+
+}else{
+  travelOutput = `
+    Inserire i dati corretti!!
+  `;
+}
 
 document.getElementById('output').innerHTML = travelOutput
+
+
+
+
+
